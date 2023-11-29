df4fa1cb6654109156f9066f9991b67335652515
Erica  Lira <104800676+ericapaiva@users.noreply.github.com>
2023-11-29 19:22:33 +0000

    Criando um padrão decorator

--- TrabalhosPD/Decorator.ts
+++ TrabalhosPD/Decorator.ts
@@ -6,54 +6,53 @@ o ingrediemte adicional pepperoni custa $0,99
 o ingrediente adicional mussarela ralado custa $2,00
 crie as classes necessárias: FrangoAssado, Pepperoni e QueijoMussarelaRalado */
 
-
 // Interface
 interface Sanduiche {
-    getDescricao(): string;
-    getCusto(): number;
+  getDescricao(): string;
+  getCusto(): number;
+}
+
+// Componente concreto
+class FrangoAssado implements Sanduiche {
+  getDescricao(): string {
+    return "Sanduíche de Frango Assado";
   }
-  
-  // Componente concreto
-  class FrangoAssado implements Sanduiche {
-    getDescricao(): string {
-      return "Sanduíche de Frango Assado";
-    }
-  
-    getCusto(): number {
-      return 4.5;
-    }
+
+  getCusto(): number {
+    return 4.5;
   }
-  
-  // Decorador concreto: Pepperoni
-  class Pepperoni implements Sanduiche {
-    constructor(private sanduiche: Sanduiche) {}
-  
-    getDescricao(): string {
-      return this.sanduiche.getDescricao() + ", Pepperoni";
-    }
-  
-    getCusto(): number {
-      return this.sanduiche.getCusto() + 0.99;
-    }
+}
+
+// Decorador concreto: Pepperoni
+class Pepperoni implements Sanduiche {
+  constructor(private sanduiche: Sanduiche) {}
+
+  getDescricao(): string {
+    return this.sanduiche.getDescricao() + ", Pepperoni";
   }
-  
-  // Decorador concreto: QueijoMussarelaRalado
-  class QueijoMussarelaRalado implements Sanduiche {
-    constructor(private sanduiche: Sanduiche) {}
-  
-    getDescricao(): string {
-      return this.sanduiche.getDescricao() + ", Queijo Mussarela Ralado";
-    }
-  
-    getCusto(): number {
-      return this.sanduiche.getCusto() + 2.0;
-    }
+
+  getCusto(): number {
+    return this.sanduiche.getCusto() + 0.99;
   }
-  
-  // Cliente
-  const sanduiche = new QueijoMussarelaRalado(new Pepperoni(new FrangoAssado()));
-  
-  // Imprimindo no console
-  console.log(sanduiche.getDescricao() + " custa $" + sanduiche.getCusto().toFixed(2));
-  
-  
+}
+
+// Decorador concreto: QueijoMussarelaRalado
+class QueijoMussarelaRalado implements Sanduiche {
+  constructor(private sanduiche: Sanduiche) {}
+
+  getDescricao(): string {
+    return this.sanduiche.getDescricao() + ", Queijo Mussarela Ralado";
+  }
+
+  getCusto(): number {
+    return this.sanduiche.getCusto() + 2.0;
+  }
+}
+
+// Cliente
+const sanduiche = new QueijoMussarelaRalado(new Pepperoni(new FrangoAssado()));
+
+// Imprimindo no console
+console.log(
+  sanduiche.getDescricao() + " custa $" + sanduiche.getCusto().toFixed(2)
+);
